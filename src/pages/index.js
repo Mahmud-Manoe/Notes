39d7051e521160c25328ce45ce4415888ba71174
Mahmud-Manoe <79903974+Mahmud-Manoe@users.noreply.github.com>
2024-07-14 16:16:24 +0700

    fix package4

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 const Notes = () => {
     return (
         <>
-            <h1>About me</h1>
+            <h1>Note about HIG</h1>
             <span>
                 Saya berasal dari Bintauna Pantai, Bintauna, Bolaang Mongondow Utara,
                 Sulawesi Utara. Sejak 2018 saya menempuh pendididkan tinggi di
